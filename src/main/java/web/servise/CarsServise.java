d5818491ad4ff73979c9b31bfe7fd54f65c68d79
Maksim <makskra2@gmail.com>
2024-05-13 19:24:15 +0300

    The method has been changed

--- src/main/java/web/servise/CarsServise.java
+++ src/main/java/web/servise/CarsServise.java
@@ -1,27 +1,36 @@
 package web.servise;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 import web.models.Car;
-
 import java.util.ArrayList;
 import java.util.List;
-@Controller
+
+@Component
 public class CarsServise {
     private List<Car> cars;
+
     {
         cars = new ArrayList<Car>();
 
-        cars.add(new Car(1,1994,"Toyota"));
-        cars.add(new Car(13,2008,"Toyota"));
-        cars.add(new Car(7,1999,"BMW"));
-        cars.add(new Car(15,2023,"Mercedes"));
-        cars.add(new Car(11,2019,"Audi"));
+        cars.add(new Car(1, 1994, "Toyota"));
+        cars.add(new Car(13, 2008, "Toyota"));
+        cars.add(new Car(7, 1999, "BMW"));
+        cars.add(new Car(15, 2023, "Mercedes"));
+        cars.add(new Car(11, 2019, "Audi"));
 
     }
-    public List<Car> getCars() {
-        return cars;
-    }
-    public Car showCar(int car) {
-        return cars.get(car);
+
+    public List<Car> getCars(Integer count) {
+        if (count == null) {
+            return cars;
+        } else if (count > 0 && count < 5) {
+            List<Car> extractedCars = new ArrayList<>();
+            for (int i = 0; i < count; i++) {
+                extractedCars.add(cars.get(i));
+            }
+            return extractedCars;
+        } else {
+            return cars;
+        }
     }
 }
